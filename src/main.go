3c7418b9981d916e05bb3443fcaa103d27db816e
apa420 <erikh.boom@gmail.com>
2020-04-07 17:54:25 +0200

    No more spaces

--- src/main.go
+++ src/main.go
@@ -1,53 +1,52 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "strings"
+    "time"
     "io/ioutil"
-    "bufio"
-    "io"
+    "bytes"
 
-	"github.com/gempir/go-twitch-irc"
+    "github.com/gempir/go-twitch-irc"
 );
 
 // Config file structs
 type Config struct {
-	Account  Account   `json:"account"`
-	Github   Github    `json:"github"`
-	Channels []Channel `json:"channels"`
+    Account  Account   `json:"account"`
+    Github   Github    `json:"github"`
+    Channels []Channel `json:"channels"`
 };
 
 type Account struct {
-	Username   string `json:"username"`
-	UserID     string `json:"userid"`
-	OauthToken string `json:"oauthToken"`
-	ClientID   string `json:"clientID"`
-	Owner      string `json:"owner"`
+    Username   string `json:"username"`
+    UserID     string `json:"userid"`
+    OauthToken string `json:"oauthToken"`
+    ClientID   string `json:"clientID"`
+    Owner      string `json:"owner"`
 };
 
 type Github struct {
-	GithubToken string `json:"oauthToken"`
+    GithubToken string `json:"oauthToken"`
     ScheduleUrl string `json:"scheduleUrl"`
 };
 
 type Channel struct {
-	ChannelName string `json:"channelName"`
+    ChannelName string `json:"channelName"`
 };
 
 type ScheduleArray struct {
-	Schedule []Schedule `json:schedule`
+    Schedule []Schedule `json:schedule`
 };
 
 type Schedule struct {
-	Title   string    `json:"title"`
-	Twitch  string    `json:"twitch"`
-	Project string    `json:"project"`
-	IntTime int64     `json:"time"`
-	Time    time.Time `json:"-"`
+    Title   string    `json:"title"`
+    Twitch  string    `json:"twitch"`
+    Project string    `json:"project"`
+    IntTime int64     `json:"time"`
+    Time    time.Time `json:"-"`
 };
 
 type Gist struct {
@@ -64,99 +63,99 @@ type ScheduleJson struct {
 
 // Bot
 type Bot struct {
-	//Config    Config
-	Client      *twitch.Client
-	Username    string
-	UserID      string
-	OauthToken  string
-	ClientID    string
-	GithubToken string
+    //Config    Config
+    Client      *twitch.Client
+    Username    string
+    UserID      string
+    OauthToken  string
+    ClientID    string
+    GithubToken string
     GistUrl     string
-	Channels    []Channel
-	Owner       string
-	NormalMsg   [20]time.Time
-	ModMsg      [100]time.Time
-	PrvMsg      string
-	PrvMsgIdx   int8
+    Channels    []Channel
+    Owner       string
+    NormalMsg   [20]time.Time
+    ModMsg      [100]time.Time
+    PrvMsg      string
+    PrvMsgIdx   int8
 };
 
 // TODO: test
 func newBot() *Bot {
-	config := loadConfig();
-	bot := &Bot{
-		Client:      twitch.NewClient(config.Account.Username, config.Account.OauthToken),
-		Username:    config.Account.Username,
-		UserID:      config.Account.UserID,
-		OauthToken:  config.Account.OauthToken,
-		ClientID:    config.Account.ClientID,
-		GithubToken: config.Github.GithubToken,
+    config := loadConfig();
+    bot := &Bot{
+        Client:      twitch.NewClient(config.Account.Username, config.Account.OauthToken),
+        Username:    config.Account.Username,
+        UserID:      config.Account.UserID,
+        OauthToken:  config.Account.OauthToken,
+        ClientID:    config.Account.ClientID,
+        GithubToken: config.Github.GithubToken,
         GistUrl:     config.Github.ScheduleUrl,
-		Channels:    config.Channels,
-		Owner:       config.Account.Owner,
-		NormalMsg:   [20]time.Time{},
-		ModMsg:      [100]time.Time{},
-		PrvMsg:      "",
-		PrvMsgIdx:   0,
-	};
-	return bot;
+        Channels:    config.Channels,
+        Owner:       config.Account.Owner,
+        NormalMsg:   [20]time.Time{},
+        ModMsg:      [100]time.Time{},
+        PrvMsg:      "",
+        PrvMsgIdx:   0,
+    };
+    return bot;
 };
 
 func check(e error) {
-	if (e != nil) {
-		panic(e);
-	}
+    if (e != nil) {
+        panic(e);
+    }
 }
 
 func loadConfig() Config {
-	jsonFile, err := os.Open("config.json");
-	check(err);
-	defer jsonFile.Close();
+    jsonFile, err := os.Open("config.json");
+    check(err);
+    defer jsonFile.Close();
 
-	var config Config;
-	json.NewDecoder(jsonFile).Decode(&config);
+    var config Config;
+    json.NewDecoder(jsonFile).Decode(&config);
 
-	return config;
+    return config;
 }
 
 func getSchedule(gistUrl string) ScheduleArray {
-	resp, err := http.Get(gistUrl);
-	check(err);
+    resp, err := http.Get(gistUrl);
+    check(err);
 
-	defer resp.Body.Close();
+    defer resp.Body.Close();
 
-	var scheduleArray ScheduleArray;
-	fmt.Println(time.Now());
-	err = json.NewDecoder(resp.Body).Decode(&scheduleArray);
-	check(err);
-	for i := 0; i < len(scheduleArray.Schedule); i++ {
-		scheduleArray.Schedule[i].Time = time.Unix(
-			scheduleArray.Schedule[i].IntTime/1000,
+    var scheduleArray ScheduleArray;
+    fmt.Println(time.Now());
+    err = json.NewDecoder(resp.Body).Decode(&scheduleArray);
+    check(err);
+    for i := 0; i < len(scheduleArray.Schedule); i++ {
+        scheduleArray.Schedule[i].Time = time.Unix(
+            scheduleArray.Schedule[i].IntTime/1000,
             scheduleArray.Schedule[i].IntTime%1000*1000*1000);
-	}
-	return scheduleArray;
+    }
+    return scheduleArray;
 }
 
 func isChannelLive(channelID string, clientID string) bool {
 
-	client := &http.Client{};
-	req, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/streams/"+channelID, nil);
-	check(err);
-	req.Header.Set("Accept", "application/vnd.twitchtv.v5+json");
-	req.Header.Set("Client-ID", clientID);
-	check(err);
+    client := &http.Client{};
+    req, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/streams/"+channelID, nil);
+    check(err);
+    req.Header.Set("Accept", "application/vnd.twitchtv.v5+json");
+    req.Header.Set("Client-ID", clientID);
+    check(err);
 
-	resp, err := client.Do(req);
-	check(err);
+    resp, err := client.Do(req);
+    check(err);
 
-	defer resp.Body.Close();
+    defer resp.Body.Close();
 
-	type T struct {
-		Stream interface{};
-	}
-	var t T;
-	err = json.NewDecoder(resp.Body).Decode(&t);
-	check(err);
-	return t.Stream != nil;
+    type T struct {
+        Stream interface{};
+    }
+    var t T;
+    err = json.NewDecoder(resp.Body).Decode(&t);
+    check(err);
+    return t.Stream != nil;
 }
 
 func updateSchedule(scheduleAddition Schedule, url string, githubOAuth string, gistUrl string) bool {
@@ -165,10 +164,9 @@ func updateSchedule(scheduleAddition Schedule, url string, githubOAuth string, g
     scheduleArray := getSchedule(gistUrl);
     scheduleArray.Schedule = append(scheduleArray.Schedule, scheduleAddition);
 
-    buffer :=
+    var buffer bytes.Buffer;
 
-    //buffer := bufio.NewWriter();
-    body := json.NewEncoder(writer).Encode(Gist {
+    json.NewEncoder(&buffer).Encode(Gist {
         Files {
             ScheduleJson {
                 ScheduleArray: scheduleArray,
@@ -176,58 +174,58 @@ func updateSchedule(scheduleAddition Schedule, url string, githubOAuth string, g
         },
     });
 
-	client := &http.Client{};
+    client := &http.Client{};
     req, err := http.NewRequest(
                      "PATCH",
                      "https://api.github.com/gists/" + strings.SplitN(url, "/", 6)[4],
-                     body);
+                     &buffer);
 
-	check(err);
-	req.Header.Set("Authorization", githubOAuth);
-	check(err);
+    check(err);
+    req.Header.Set("Authorization", githubOAuth);
+    check(err);
 
-	//resp, err := client.Do(req);
-	check(err);
+    resp, err := client.Do(req);
+    check(err);
 
-	defer resp.Body.Close();
+    defer resp.Body.Close();
 
-	type T struct {
-		Stream interface{};
-	};
-	var t T;
-	err = json.NewDecoder(resp.Body).Decode(&t);
+    type T struct {
+        Stream interface{};
+    };
+    var t T;
+    err = json.NewDecoder(resp.Body).Decode(&t);
     fmt.Println(t);
 
-    str, err := ioutil.ReadAll(resp.Body);
-    fmt.Printf("%s", str);
+    //str, err := ioutil.ReadAll(resp.Body);
+    //fmt.Printf("%s", str);
 
     if (err != nil) {
         fmt.Printf("can't decode json string `%s`: %s", t, err);
     };
 
-	check(err);
-	return t.Stream != nil;
+    check(err);
+    return t.Stream != nil;
 }
 
 func connectToChannels(client *twitch.Client, channels []Channel) {
-	for i := 0; i < len(channels); i++{
-		client.Join(channels[i].ChannelName);
-		client.Say(channels[i].ChannelName, ":)");
-	}
+    for i := 0; i < len(channels); i++{
+        client.Join(channels[i].ChannelName);
+        client.Say(channels[i].ChannelName, ":)");
+    }
 }
 
 func sendMessage(target string, message string, bot *Bot) {
-	if (message[0] == '.' || message[0] == '/') {
-		message = ". " + message;
-	}
-	if (len(message) > 247) {
-		message = message[0:247];
-	}
-	if (bot.PrvMsg == message) {
-		message += " \U000E0000";
-	}
-	bot.Client.Say(target, message);
-	bot.PrvMsg = message;
+    if (message[0] == '.' || message[0] == '/') {
+        message = ". " + message;
+    }
+    if (len(message) > 247) {
+        message = message[0:247];
+    }
+    if (bot.PrvMsg == message) {
+        message += " \U000E0000";
+    }
+    bot.Client.Say(target, message);
+    bot.PrvMsg = message;
 }
 
 func banUser(target string, user string, bot *Bot) {
@@ -235,30 +233,30 @@ func banUser(target string, user string, bot *Bot) {
 }
 
 func handleMessage(message twitch.PrivateMessage, bot *Bot) {
-	if (message.Message[0] == '/') {
+    if (message.Message[0] == '/') {
 
-		if (throttleNormalMessage(bot)) {
-			return;
-		}
+        if (throttleNormalMessage(bot)) {
+            return;
+        }
 
-		bot.NormalMsg[bot.PrvMsgIdx] = time.Now();
-		bot.PrvMsgIdx = (bot.PrvMsgIdx + 1) % 20;
+        bot.NormalMsg[bot.PrvMsgIdx] = time.Now();
+        bot.PrvMsgIdx = (bot.PrvMsgIdx + 1) % 20;
 
-		commandName := strings.SplitN(message.Message, " ", 2)[0][1:];
+        commandName := strings.SplitN(message.Message, " ", 2)[0][1:];
 
-		switch commandName {
-		case "":
-			sendMessage(message.Channel, "Why, hello there :)", bot);
-		case "get":
-			if (message.Tags["display-name"] == bot.Owner) {
-				// Do http request
-				scheduleArray := getSchedule(bot.GistUrl);
-				fmt.Println(scheduleArray);
+        switch commandName {
+        case "":
+            sendMessage(message.Channel, "Why, hello there :)", bot);
+        case "get":
+            if (message.Tags["display-name"] == bot.Owner) {
+                // Do http request
+                scheduleArray := getSchedule(bot.GistUrl);
+                fmt.Println(scheduleArray);
 
-				sendMessage(message.Channel, "Sent request", bot);
-			}
-		case "update":
-			if (message.Tags["display-name"] == bot.Owner) {
+                sendMessage(message.Channel, "Sent request", bot);
+            }
+        case "update":
+            if (message.Tags["display-name"] == bot.Owner) {
                 // Test schedule message
                 schedule := Schedule{
                     Title:   "This is a nice test title",
@@ -268,21 +266,21 @@ func handleMessage(message twitch.PrivateMessage, bot *Bot) {
                     Time:    time.Now(),
                 };
 
-				if (updateSchedule(schedule, bot.GistUrl, bot.GithubToken, bot.GistUrl)) {
-				    sendMessage(message.Channel, "Request succeeded!", bot);
+                if (updateSchedule(schedule, bot.GistUrl, bot.GithubToken, bot.GistUrl)) {
+                    sendMessage(message.Channel, "Request succeeded!", bot);
                 } else {
-				    sendMessage(message.Channel, "Request failed!", bot);
+                    sendMessage(message.Channel, "Request failed!", bot);
                 }
 
-			}
+            }
 case "isLive":
-			if (message.Tags["display-name"] == bot.Owner) {
-				if (isChannelLive(message.RoomID, bot.ClientID)) {
-					sendMessage(message.Channel, "Live :)", bot);
-				} else {
-					sendMessage(message.Channel, "Offline :/", bot);
-				}
-			}
+            if (message.Tags["display-name"] == bot.Owner) {
+                if (isChannelLive(message.RoomID, bot.ClientID)) {
+                    sendMessage(message.Channel, "Live :)", bot);
+                } else {
+                    sendMessage(message.Channel, "Offline :/", bot);
+                }
+            }
         case "echo":
             if (message.Tags["display-name"] == bot.Owner) {
                 if (len(strings.SplitN(message.Message, " ", 2)) > 1) {
@@ -307,45 +305,45 @@ case "isLive":
             }
         default:
             sendMessage(message.Channel, "Soon ™", bot);
-		}
-	}
+        }
+    }
 }
 
 func throttleNormalMessage(bot *Bot) bool {
 
-	if (bot.NormalMsg[(bot.PrvMsgIdx+19)%20].Add(1500 * time.Millisecond).After(time.Now())) {
-		return true;
-	}
-	if (bot.NormalMsg[bot.PrvMsgIdx].Add(30 * time.Second).After(time.Now())) {
-		return true;
-	}
-	return false;
+    if (bot.NormalMsg[(bot.PrvMsgIdx+19)%20].Add(1500 * time.Millisecond).After(time.Now())) {
+        return true;
+    }
+    if (bot.NormalMsg[bot.PrvMsgIdx].Add(30 * time.Second).After(time.Now())) {
+        return true;
+    }
+    return false;
 }
 
 func main() {
-	bot := newBot();
+    bot := newBot();
 
-	connectToChannels(bot.Client, bot.Channels);
+    connectToChannels(bot.Client, bot.Channels);
 
-	bot.Client.OnConnect(func() {
-		fmt.Println("connected");
-	})
+    bot.Client.OnConnect(func() {
+        fmt.Println("connected");
+    })
 
-	bot.Client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		channelID := message.Tags["room-id"];
-		if (channelID == "") {
-			fmt.Printf("Missing room-id tag in message");
-			return;
-		}
+    bot.Client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+        channelID := message.Tags["room-id"];
+        if (channelID == "") {
+            fmt.Printf("Missing room-id tag in message");
+            return;
+        }
 
-		if (message.Tags["user-id"] == bot.UserID) {
-			return;
-		}
-		handleMessage(message, bot);
+        if (message.Tags["user-id"] == bot.UserID) {
+            return;
+        }
+        handleMessage(message, bot);
 
-	})
+    })
 
-	fmt.Println("Finished loading");
-	err := bot.Client.Connect();
-	check(err);
+    fmt.Println("Finished loading");
+    err := bot.Client.Connect();
+    check(err);
 }
